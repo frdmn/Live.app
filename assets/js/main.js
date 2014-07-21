1cccfede9d58969398820b08743499b6d9902796
Jonas Friedann <j@frd.mn>
2014-07-21 19:57:02 +0200

    Add basic modal view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
 $(function() {
 	/* Sidebar toggle */ 
 	$("#menu-toggle").click(function(e) {
-	    e.preventDefault();
-	    $("#wrapper").toggleClass("active");
+		e.preventDefault();
+		$("#wrapper").toggleClass("active");
+	});
+
+	/* Modal */ 
+
+	// Open modal
+	$(".open-modal").click(function() {
+		$(".modal").addClass("modal--open");
+	});
+
+	// Close modal
+	$(".close-modal").click(function() {
+		$(".modal").removeClass("modal--open");
+	});
+
+	// Listen on ESC to close modal
+	$(document).keyup(function (e) {
+		if (e.which == 27) {
+			if ($(".modal").hasClass("modal--open")) {
+				$(".modal").removeClass("modal--open");
+			}
+		}
 	});
 });
 
