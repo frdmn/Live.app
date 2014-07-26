8b4ede51d7b3d5b44e69c5cbd7d4dbfe635d1782
Jonas Friedann <j@frd.mn>
2014-07-26 23:04:03 +0200

    Adjust Gruntfile.js for node-webkit-builder task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,28 @@
+var parseBuildPlatforms = function(argumentPlatform) {
+    // this will make it build no platform when the platform option is specified
+    // without a value which makes argumentPlatform into a boolean
+    var inputPlatforms = argumentPlatform || process.platform + ";" + process.arch;
+
+    // Do some scrubbing to make it easier to match in the regexes bellow
+    inputPlatforms = inputPlatforms.replace("darwin", "mac");
+    inputPlatforms = inputPlatforms.replace(/;ia|;x|;arm/, "");
+
+    var buildAll = /^all$/.test(inputPlatforms);
+
+    var buildPlatforms = {
+        mac: /mac/.test(inputPlatforms) || buildAll,
+        win: /win/.test(inputPlatforms) || buildAll,
+        linux32: /linux32/.test(inputPlatforms) || buildAll,
+        linux64: /linux64/.test(inputPlatforms) || buildAll
+    };
+
+    return buildPlatforms;
+};
+
 module.exports = function(grunt) {
+    var buildPlatforms = parseBuildPlatforms(grunt.option('platforms'));
+    var currentVersion = grunt.file.readJSON('package.json').version;
+
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
@@ -151,9 +175,22 @@ module.exports = function(grunt) {
                     spawn: false
                 }
             }
+        },
+        nodewebkit: {
+            options: {
+                build_dir: './build',
+                mac: buildPlatforms.mac,
+                win: buildPlatforms.win,
+                linux32: buildPlatforms.linux32,
+                linux64: buildPlatforms.linux64
+            },
+            src: ['./assets/js/*.js', './assets/css/*.css', './settings.json', './node_modules/**', '!./node_modules/**', './index.html', './package.json', './README.md' ]
         }
     });
 
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
     grunt.registerTask('default', ['sass:build', 'autoprefixer', 'concat', 'uglify', 'imagemin']);
     grunt.registerTask('dev', ['connect', 'watch']);
+    grunt.registerTask('build', ['default', 'nodewebkit']);
 };
